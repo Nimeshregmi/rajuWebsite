add157846e5e81b918baf91c43356a50bc6bd12f
Nimesh <regminimesh7@gmail.com>
2024-05-22 15:04:11 +0545

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Profilepic from "../../public/images/profile/developer-pic-1.png";
 import AnimatedText from "@/components/AnimatedText";
 import Link from "next/link";
 import { LinkArrow } from "@/components/Icons";
-import Hireme from "@/components/Hireme";
+// import Hireme from "@/components/Hireme";
 import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg"
 
 export default function Home() {
@@ -22,14 +22,13 @@ export default function Home() {
               
             </div>
             <div className="w-1/2 my-auto flex flex-col items-center self-center lg:w-full lg:text-center  ">
-              <AnimatedText text="Turning Vision Into Reality With Code And Design. " classname="!text-left xl:!text-5xl 
+              <AnimatedText text="Turning Vision Into Reality With Editing And Design. " classname="!text-left xl:!text-5xl 
               lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl  
               "/>
               <p className="text-left text-base font-medium my-4 md:text-sm sm:text-xs">
-                As a skilled full-stack developer, I am dedicated to turning
-                ideas into innovative web applications. Explore my latest
-                projects and articles, showcasing my expertise in React.js and
-                web development.
+                As a skilled Video Editor, I am dedicated to turning
+                ideas into innovative Video. Explore my latest
+                projects and articles, showcasing my expertise in Video Editing and UI/UX.
               </p>
               <div className="flex items-center self-start mt-4 lg:self-center">
                 <Link className="px-3 text-lg font-semibold rounded-2xl dark:bg-light bg-dark text-light dark:text-dark items-center p-2.5
@@ -45,7 +44,7 @@ export default function Home() {
             </div>
           </div>
         </Profilelayout>
-        <Hireme/>
+        {/* <Hireme/> */}
         <div className="absolute right-8 bottom-8 inline-block w-24 md:hidden">
           <Image src={lightBulb} alt="Nimesh" className="w-full h-auto"/>
         </div>
--- src/components/Hireme.tsx
+++ src/components/Hireme.tsx
@@ -7,7 +7,7 @@ const Hireme = () => {
   return (
     <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden  md:right-8 md:absolute md:left-auto md:top-2 md:bottom-auto">
         <div className=" w-48 md:w-24 h-auto flex items-center justify-center relative">
-            <CircularText className="fill-dark dark:fill-light animate-spin-slow"/>
+            {/* <CircularText className="fill-dark dark:fill-light animate-spin-slow"/> */}
             <Link href="mailto:abcd@gmail.com" className="flex items-center justify-center absolute left-1/2 top-1/2  -translate-x-1/2 -translate-y-1/2 bg-dark dark:bg-light text-light dark:text-dark shadow-md border border-solid dark:border-light border-dark w-20 h-20 rounded-full font-semibold dark:hover:bg-dark  hover:bg-light hover:text-dark dark:hover:text-light
             md:w-12 md:h-12 md:text-[10px]
             ">Hire Me</Link>
