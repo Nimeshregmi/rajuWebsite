3d032df8fec34da5f1589f6065479d27ad8fa4b9
Nimesh <regminimesh7@gmail.com>
2024-08-10 09:49:50 +0545

    d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,79 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Script from "next/script";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Montserrat({ subsets: ["latin"], variable: "--font-mont" });
 
 export const metadata: Metadata = {
   title: "RajuBasnet",
-  description: "Create Your design",
+  description: "Discover stunning visual creations with Raju Basnet, a leading video and graphics designer specializing in innovative video production, dynamic motion graphics, and cutting-edge graphic design. Explore captivating videos, unique branding solutions, and modern web design that elevate your brand.",
+  keywords: [
+    "beingraju",
+    "raju",
+    "basnet",
+    "video",
+    "editor",
+    "graphics",
+    "designer",
+    "nepal",
+    "professional video editor and graphics designer",
+    "Motion Graphics",
+    "Visual Storytelling",
+    "Brand Identity Design",
+    "Video Production",
+    "Adobe Creative Suite",
+    "3D Animation",
+    "Logo Design",
+    "Color Grading",
+    "Typography",
+    "Visual Effects (VFX)",
+    "User Interface (UI) Design",
+    "User Experience (UX) Design",
+    "Social Media Graphics",
+    "Video Editing",
+    "Infographic Design",
+    "Cinematography",
+    "Digital Illustration",
+    "Branding Strategy",
+    "Post-Production",
+    "Responsive Web Design",
+    "Package Design",
+    "Storyboarding",
+    "Drone Videography",
+    "Print Design",
+    "Corporate Video",
+    "Animated Explainer Videos",
+    "Photo Retouching",
+    "Digital Marketing Visuals",
+    "Compositing",
+    "Sound Design",
+    "Vector Graphics",
+    "Event Videography",
+    "Book Cover Design",
+    "Augmented Reality (AR) Design",
+    "Virtual Reality (VR) Design",
+    "Podcast Cover Art",
+    "Video Transitions",
+    "Kinetic Typography",
+    "Album Artwork",
+    "360-Degree Video",
+    "Whiteboard Animation",
+    "E-commerce Product Photography",
+    "Data Visualization",
+    "Aerial Photography",
+    "Stop Motion Animation",
+    "Architectural Visualization",
+    "Character Design",
+    "Green Screen Editing",
+    "Digital Painting",
+    "Title Sequence Design",
+    "Isometric Design",
+    "Minimalist Design",
+    "Flat Design",
+    "Vintage/Retro Design",
+    "Responsive Logo Design",
+  ],
 };
 
 export default function RootLayout({
@@ -20,9 +86,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-light dark:bg-dark w-full px-0 mx-0
-       min-h-screen`}>
-        <Script  id="theme" strategy="beforeInteractive">
+      <body
+        className={`${inter.className} bg-light dark:bg-dark w-full px-0 mx-0
+       min-h-screen`}
+      >
+        <Script id="theme" strategy="beforeInteractive">
           {`
           if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
             document.documentElement.classList.add('dark')
@@ -33,7 +101,7 @@ export default function RootLayout({
         <Navbar />
         <Analytics />
         <main className="pt-16">{children}</main>
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
